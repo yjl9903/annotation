902ad776046a0d51f7884bd70c40c73a7f624f00
yjl9903 <yjl9903@onekuma.cn>
2025-05-26 01:38:58 +0800

    feat: 132B

--- generator.cpp
+++ generator.cpp
@@ -19,36 +19,15 @@ int main(const int argc, char **argv) {
   printf("Output: %s\n", filename.c_str());
   freopen(filename.c_str(), "w", stdout);
 
-  int n = opt<int>("n");
-  int t = opt<int>("t");
-  int k = opt<int>("k");
-  int q = opt<int>("q");
+  int m = output;
+  int n = rnd.next(1, (int)5e7);
 
-  printf("%d\n", t);
-  for (int i = 0; i < t; i++) {
-    printf("%d %d\n", n, k);
-    for (int i = 0; i < n; i++) {
-      printf("%c", 'a' + rnd.next(0, 25));
+  printf("%d %d\n", m, n);
+  for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {
+      printf("%c", '0' + rnd.next(0, 9));
     }
     puts("");
-
-    vector<int> bound;
-    for (int i = 0; i < n; i++) {
-      bound.push_back(i + 1);
-    }
-    shuffle(bound.begin(), bound.end());
-    bound.resize(k - 1);
-    sort(bound.begin(), bound.end());
-    for (int i = 0; i < k; i++) {
-      printf("%d%c", i == 0 ? 1 : bound[i - 1], " \n"[i + 1 == k]);
-    }
-    for (int i = 0; i < k; i++) {
-      printf("%d%c", i == k - 1 ? n : bound[i], " \n"[i + 1 == k]);
-    }
-    printf("%d\n", q);
-    for (int i = 0; i < q; i++) {
-      printf("%d%c", rnd.next(1, n), " \n"[i + 1 == q]);
-    }
   }
 
   return 0;
