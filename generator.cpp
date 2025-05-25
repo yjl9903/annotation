b31d63fd02c4644e2f97b3a5e7c353d57dd7927d
yjl9903 <yjl9903@onekuma.cn>
2025-05-26 01:20:10 +0800

    feat: 1878D

--- generator.cpp
+++ generator.cpp
@@ -19,39 +19,36 @@ int main(const int argc, char **argv) {
   printf("Output: %s\n", filename.c_str());
   freopen(filename.c_str(), "w", stdout);
 
-  int n = output;
-  vector<int> p(n);
-  for (int i = 0; i < n; i++) {
-    p[i] = i + 1;
-  }
-  shuffle(p.begin(), p.end());
-  // vector<int> p {1, 2, 3, 4, 7, 5, 6};
+  int n = opt<int>("n");
+  int t = opt<int>("t");
+  int k = opt<int>("k");
+  int q = opt<int>("q");
 
-  n = p.size();
-  vector<int> a(n);
-
-  // for (int i = 0; i < n; i++) {
-  //   printf("%d%c", p[i], " \n"[i == n - 1]);
-  // }
-
-  for (int i = 0; i < n; i++) {
-    bool found = false;
-    for (int j = 0; j < n; j++) {
-      if (p[i] < p[j] && !vis[j]) {
-        found = true;
-        a[i] = j + 1;
-        vis[j] = true;
-        break;
-      }
+  printf("%d\n", t);
+  for (int i = 0; i < t; i++) {
+    printf("%d %d\n", n, k);
+    for (int i = 0; i < n; i++) {
+      printf("%c", 'a' + rnd.next(0, 25));
     }
-    if (!found) {
-      a[i] = -1;
-    }
-  }
+    puts("");
 
-  printf("%d\n", n);
-  for (int i = 0; i < n; i++) {
-    printf("%d%c", a[i], " \n"[i == n - 1]);
+    vector<int> bound;
+    for (int i = 0; i < n; i++) {
+      bound.push_back(i + 1);
+    }
+    shuffle(bound.begin(), bound.end());
+    bound.resize(k - 1);
+    sort(bound.begin(), bound.end());
+    for (int i = 0; i < k; i++) {
+      printf("%d%c", i == 0 ? 1 : bound[i - 1], " \n"[i + 1 == k]);
+    }
+    for (int i = 0; i < k; i++) {
+      printf("%d%c", i == k - 1 ? n : bound[i], " \n"[i + 1 == k]);
+    }
+    printf("%d\n", q);
+    for (int i = 0; i < q; i++) {
+      printf("%d%c", rnd.next(1, n), " \n"[i + 1 == q]);
+    }
   }
 
   return 0;
