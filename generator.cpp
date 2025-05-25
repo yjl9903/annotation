399b6a90c7d482aa80a13bff7a95f90fe80fcc6e
yjl9903 <yjl9903@onekuma.cn>
2025-05-26 01:51:32 +0800

    feat: 374B

--- generator.cpp
+++ generator.cpp
@@ -19,16 +19,11 @@ int main(const int argc, char **argv) {
   printf("Output: %s\n", filename.c_str());
   freopen(filename.c_str(), "w", stdout);
 
-  int m = output;
-  int n = rnd.next(1, (int)5e7);
-
-  printf("%d %d\n", m, n);
-  for (int i = 0; i < m; i++) {
-    for (int i = 0; i < m; i++) {
-      printf("%c", '0' + rnd.next(0, 9));
-    }
-    puts("");
+  int n = output;
+  for (int i = 1; i <= n; i++) {
+    putchar('0' + rnd.next(1, 9));
   }
+  puts("");
 
   return 0;
 }
